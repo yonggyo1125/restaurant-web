3f83a6ed3cc5da040d75565edf2161b6add9395f
yonggyo1125 <yonggyo00@kakao.com>
2025-02-11 12:21:07 +0900

    .

--- src/app/restaurant/components/CategoryTabs.tsx
+++ src/app/restaurant/components/CategoryTabs.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
+'use client'
+import React, { useState, useLayoutEffect } from 'react'
+import { getCategories } from '../services/actions'
 
 const CategoryTabs = ({ categories, onClick }) => {
+  const [items, setItems] = useState<string[]>()
+
+  useLayoutEffect(() => {
+    ;(async () => {
+      const data = await getCategories()
+      setItems(data)
+    })()
+  }, [])
+
   return <></>
 }
 
